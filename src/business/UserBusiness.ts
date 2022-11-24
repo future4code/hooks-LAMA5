16d5afabf8f6671905611ecc18e36407d7cf12c2
unknown <wictor.feitosa@gmail.com>
2022-11-23 23:37:30 -0300

    mexendo nos arquivos userbusiness, user controller, authenticator, para criação do login

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,57 +1,73 @@
-import { UserInputDTO, LoginInputDTO } from "../model/User";
+import { UserInputDTO, LoginInputDTO, User, UserRole } from "../model/User";
 import { UserDatabase } from "../data/UserDatabase";
 import { IdGenerator } from "../services/IdGenerator";
 import { HashManager } from "../services/HashManager";
 import { Authenticator } from "../services/Authenticator";
+import { BaseError, InvalidEmail, InvalidName } from "../error/BaseError";
+
+const idGenerator = new IdGenerator();
+const hashManager = new HashManager();
+const userDatabase = new UserDatabase();
+const authenticator = new Authenticator();
 
 export class UserBusiness {
+  async createUser(input: UserInputDTO) {
+    try {
+      const { name, email, password, role } = input;
+      if (!name || !email || !password || !role) {
+        throw new BaseError(
+          400,
+          'Preencha os campos "name", "email", e "password"'
+        );
+      }
 
-    async createUser(user: UserInputDTO) {
+      if (name.length < 3) {
+        throw new InvalidName();
+      }
 
-        try {
-            const {name, email, password, role } = user;
+      if (!email.includes("@")) {
+        throw new InvalidEmail();
+      }
 
-            if (!name || !email || !password || !role){
-                throw new Error(
-                400, 'Preencha os campos "name", "email", e "password"'
-                )
-            }
-        } catch (error) {
-            
-        }
+      const id: string = idGenerator.generateId();
 
+      const hashPassword: string = await hashManager.hash(input.password);
 
+      const user: User = {
+        id,
+        name,
+        email,
+        password: hashPassword,
+        role: UserRole.NORMAL,
+      };
+      
+      await userDatabase.createUser(id, email, name, password, role);
 
-        const idGenerator = new IdGenerator();
-        const id = idGenerator.generate();
+      const accessToken = authenticator.generateToken({ id, role: user.role });
+      return accessToken;
+    } catch (error) {}
+  }
 
-        const hashManager = new HashManager();
-        const hashPassword = await hashManager.hash(user.password);
+  async getUserByEmail(user: LoginInputDTO) {
+    const userDatabase = new UserDatabase();
+    const userFromDB = await userDatabase.getUserByEmail(user.email);
 
-        const userDatabase = new UserDatabase();
-        await userDatabase.createUser(id, user.email, user.name, hashPassword, user.role);
+    const hashManager = new HashManager();
+    const hashCompare = await hashManager.compare(
+      user.password,
+      userFromDB.getPassword()
+    );
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id, role: user.role });
+    const authenticator = new Authenticator();
+    const accessToken = authenticator.generateToken({
+      id: userFromDB.getId(),
+      role: userFromDB.getRole(),
+    });
 
-        return accessToken;
+    if (!hashCompare) {
+      throw new Error("Invalid Password!");
     }
 
-    async getUserByEmail(user: LoginInputDTO) {
-
-        const userDatabase = new UserDatabase();
-        const userFromDB = await userDatabase.getUserByEmail(user.email);
-
-        const hashManager = new HashManager();
-        const hashCompare = await hashManager.compare(user.password, userFromDB.getPassword());
-
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
-
-        if (!hashCompare) {
-            throw new Error("Invalid Password!");
-        }
-
-        return accessToken;
-    }
+    return accessToken;
+  }
 }
--- src/error/BaseError.ts
+++ src/error/BaseError.ts
@@ -1,6 +1,35 @@
-export abstract class BaseError extends Error {
-    constructor(message: string, public code: number) {
+export class BaseError extends Error {
+    constructor(statusCode: number, message: string) {
       super(message);
     }
   }
-  
+
+export class InvalidName extends BaseError{
+  constructor(){
+    super(400, "Nome inválido")
+  }
+}
+
+export class InvalidEmail extends BaseError{ 
+  constructor(){
+      super(400, "Email inválido")
+  }
+}
+
+export class InvalidPassword extends BaseError{ 
+  constructor(){
+      super(400, "Senha inválida")
+  }
+}
+
+export class UserNotFound extends BaseError{ 
+  constructor(){
+      super(404, "Usuário não encontrado")
+  }
+}
+
+export class Unauthorized extends BaseError{ 
+  constructor(){
+      super(401, "Usuário não autorizado")
+  }
+}
